2a280417a712b13ddfd517abadb3a57a7d9312ce
ksenofonus <danilova.ka88@yandex.ru>
2024-08-07 14:59:18 +0400

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let i=0; i < str.length; i++) {
+    bracketsConfig.forEach((brackets) => {
+         brackets.forEach((item, itemindex) => {
+             let l = arr.length-1
+             if (item === str[i]) {
+                if (brackets[0] === brackets[1]) {
+                  if (itemindex === 0) {
+                    arr[l] !== str[i] ? arr.push(str[i]) : arr.pop();
+                  }
+                } else if (brackets[0] !== brackets[1]) {
+                    if (itemindex === 0) {
+                       arr.push(str[i]);
+                    } else if (itemindex === 1) {
+                          arr[l] === brackets[0] ? arr.pop() : arr.push(str[i]);
+                        }
+                }
+              }
+             }
+      )
+  })}
+  return (!arr.length) ? true :  false
 }
